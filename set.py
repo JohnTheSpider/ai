4987f76df9b3834d08eb6b1483c52b770e30fcee
john <54205453+JohnTheSpider@users.noreply.github.com>
2019-09-09 11:31:17 +0300

    Прост

--- set.py
+++ set.py
@@ -2,45 +2,15 @@ import numpy as np
 from tkinter import *
 from util.color import Color
 
-canvas_wight = 8
-canvas_height = 8
-brush_size = 10
-color = "black"
-
-def paint(event):
-    global brush_size
-    global color
-    x1 = event.x - brush_size
-    x2 = event.x + brush_size
-    y1 = event.y - brush_size
-    y2 = event.y + brush_size
-    w.create_rectangle(x1,y1,x2,y2,fill=color,outline=color)
-
-
-
-root = Tk()
-root.title("Paint для смайликов(грустный или веселый)")
-#root.minsize(700,700)
-#root.geometry("700x700")
-w = Canvas(root, width=canvas_wight,height=canvas_height,bg="white")
-#w.scale(700, 700)
-w.bind("<B1-Motion>", paint)
-w.grid(row=2, column=0,
-       columnspan=7,padx=5,pady=5, sticky=E+W+S+N)
-w.columnconfigure(6, weight=1)
-w.rowconfigure(2, weight=1)
-w.pack
-
-root.mainloop()
 
 
 def sigmoid(x):
     return  1 / (1 + np.exp(-x))
 
 
-training_inputs = np.array([[w],
-                            ])
-training_outputs = np.array([[1]]).T
+training_inputs = np.array([[0,1,1],
+                            [1,1,1],[1,0,1][0,0,1]])
+training_outputs = np.array([[0,1,1,0]]).T
 np.random.seed(1)
 synaptic_weights = 2 * np.random.random((3,1)) - 1
 print ("Случайные веса: ")
